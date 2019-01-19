f3544a1b92b2e47ed2d7edb8326758d7905e6a52
Kevin <k.cuison@gmail.com>
2019-01-19 15:25:19 -0800

    mongodb connected

---
+++ db/db.js
@@ -0,0 +1,24 @@
+//CONNECTING TO MONGO TO OUR DATABASE
+
+const mongoose = require('mongoose');
+
+//name our database
+const connectionString = 'mongodb://localhost/dog';
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+})
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${connectionString}.`)
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Mongoose ${err}.`)
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log(`Mongoose disconnected from ${err}.`)
+});
